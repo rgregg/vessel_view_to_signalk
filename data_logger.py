1e95f07ceb12e60f7f39e037dbd6c0623dd98033
Ryan Gregg <github@ryangregg.com>
2024-07-28 23:07:17 -0700

    Write out data automatically every 1 second when data is changing

--- data_logger.py
+++ data_logger.py
@@ -1,4 +1,5 @@
 import csv
+import threading
 from datetime import datetime
 
 class CSVLogger:
@@ -6,11 +7,15 @@ class CSVLogger:
         self.filename = filename
         self.fieldnames = fieldnames
         self.data = {field: None for field in fieldnames}
+        self.timer = None
         
         # Create the CSV file and write the header
         with open(self.filename, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             writer.writeheader()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.log_to_csv()
     
     def update_properties(self, **kwargs):
         for key, value in kwargs.items():
@@ -20,8 +25,12 @@ class CSVLogger:
     def update_property(self, key, value):
         self.data[key] = value
         self.data["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if self.timer is None:
+            self.timer = threading.Timer(1.0, self.log_to_csv)
+            self.timer.start()
     
     def log_to_csv(self):
         with open(self.filename, 'a', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             writer.writerow(self.data)
+        self.timer = None
